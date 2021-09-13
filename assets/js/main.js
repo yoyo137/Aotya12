9c3d7b6951b6d4033527aeb875f1656fbb17a98f
yoyo137 <89888576+yoyo137@users.noreply.github.com>
2021-09-13 13:31:25 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ const app = Vue.createApp({
           q1: '空',
           // q2: 'かかか',
           // q3: 'ききき',
-        }
+        },
         stage4: {
           q1: 'ナフ王のたから',
           // q2: 'かかか',
@@ -53,7 +53,7 @@ const app = Vue.createApp({
           false, // 3-1
           // false, // 3-2
           // false, // 3-3
-        ]
+        ],
          stage4: [
           false, 
         ]
